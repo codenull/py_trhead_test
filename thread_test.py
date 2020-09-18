c4f0f614ec8d5915133cd913f17b48a781be401c
codenull <code.null@yandex.ru>
2020-09-19 00:57:23 +1000

    Добавил файл расчета с использованием стандартного модуля работы с потоками.

---
+++ thread_test.py
@@ -0,0 +1,38 @@
+from threading import Thread
+from math import  sin, cos
+from array import array
+
+class MyThread(Thread):
+
+    def __init__(self, name, array):
+        Thread.__init__(self)
+        self.name = name
+        self.array = array
+
+    def run(self):
+        for i, val in self.array:
+            self.array[i] = self.calc(val, i)
+
+    def calc(self, value, i):
+        result = (value *
+                  sin(0.2 + i / 5.0) *
+                  cos(0.2 + i / 5.0) *
+                  cos(0.4 + i / 2.0));
+        return result
+
+
+def create_array(len):
+    return array('f', [1]) * len
+
+
+def main():
+
+    arr = create_array(1000)
+
+    print("Start")
+    print(create_array(10))
+
+
+if __name__ == '__main__':
+    main()
+
